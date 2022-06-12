186b3e38b46c5b61bcaa619b6acc7ee7d3ba414a
petarpetrov <ipetrovbg@gmail.com>
2022-06-12 14:12:15 +0300

    remove test

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -14,20 +14,3 @@ async fn main() -> Result<(), Error> {
 async fn hello(event: Value, _: Context) -> Result<Value, Error> {
     Ok(event)
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use serde_json::json;
-
-    #[tokio::test]
-    async fn hello_handles() {
-        let event = json!({
-            "answer": 42
-        });
-        assert_eq!(
-            hello(event.clone(), Context::default()).await.expect("expected Ok(_) values"),
-            event
-        )
-    }
-}
